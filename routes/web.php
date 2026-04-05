4292c27aa9cbdf19109fb8ea443a3eb6362f23ba
jhondhel148035 <jho-jhondhel@example.com>
2026-04-06 00:49:36 +0800

    Fresh and clean submission for Jhondhel

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/showcases', function () {
+    return view('showcases');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/formtest', function () {
+    $emails = session()->get('$emails', []);
+
+    return view('formtest', [
+        'emails' => $emails,
+    ]);
+});
+
+Route::post('/formtest', function () {
+    $email = request('email');
+
+    session()->push('$emails', $email);
+
+    return redirect('/formtest');
+});
+
+Route::get('/delete-emails', function () {
+    session()->forget('$emails');
+    return redirect('/formtest');
+});
